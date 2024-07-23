41cf58cccade899a47fd6a5a36028987e306faf0
Daniel Bigos <daniel.bigos@icloud.com>
2024-07-23 11:26:13 +0200

    feat: add signing and recovering

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+import { Wallet, ethers } from 'ethers';
+
+const message = "Hello World";
+
+let wallet = Wallet.createRandom();
+
+let address = await wallet.getAddress();
+
+console.log("Wallet address: " + address);
+
+let hash = ethers.utils.hashMessage(message);
+console.log("Hash: " + hash);
+
+let signature = await wallet.signMessage(message);
+const { r, s, v } = ethers.utils.splitSignature(signature);
+
+console.log("Signature: " + signature + ", v: " + v + ", r: " + r + ", s: " + s);
+
+let recovered = ethers.utils.verifyMessage(message, signature);
+
+console.log("Recovered: " + recovered);
+
+console.assert(address, recovered);
